5914b6add9d8e97533c38503e565a0dcb10d3fb9
pofider <honza.pofider@seznam.cz>
2014-02-13 19:45:01 +0100

    drastic ui changes

--- extension/scripts/public/js/scripts.detail.view.js
+++ extension/scripts/public/js/scripts.detail.view.js
@@ -1,13 +1,15 @@
 ﻿define(["marionette", "codemirror", "core/view.base", "core/codeMirrorBinder"], function(Marionette, CodeMirror, ViewBase, codeMirrorBinder) {
     return ViewBase.extend({
         template: "scripts-detail",
-        className : 'full',
 
         initialize: function () {
             this.listenTo(this.model, "sync", this.render);
         },
 
         onDomRefresh: function () {
+
+            var top = $("#contentWrap").position().top;
+            
             this.contentCodeMirror = CodeMirror.fromTextArea(this.$el.find("#contentArea")[0], {
                 mode: "javascript",
                 height: "350px",
@@ -22,6 +24,7 @@
             codeMirrorBinder(this.model, "content", this.contentCodeMirror);
             
             $(this.contentCodeMirror.getWrapperElement()).addClass(this.$el.find("#contentArea").attr('class'));
+            $(this.contentCodeMirror.getWrapperElement()).css("margin-top", top);
         },
    });
 });
--- extension/templates/public/js/template.detail.view.js
+++ extension/templates/public/js/template.detail.view.js
@@ -1,35 +1,49 @@
 ﻿define(["jquery", "app", "codemirror", "core/utils", "core/view.base", "core/codeMirrorBinder"],
-    function ($, app, CodeMirror, Utils, LayoutBase, codeMirrorBinder) {
+    function($, app, CodeMirror, Utils, LayoutBase, codeMirrorBinder) {
 
         return LayoutBase.extend({
             template: "template-detail",
             htmlCodeMirror: null,
             helpersCodeMirror: null,
+            className : 'full',
 
-            initialize: function () {
+            initialize: function() {
                 var self = this;
-                
-                this.listenTo(this.model, "sync", function () {
+
+                this.listenTo(this.model, "sync", function() {
                     self.render();
                 });
             },
 
-            onDomRefresh: function () {
+            onDomRefresh: function() {
                 var self = this;
+
+                var topHtml = $("#htmlWrap").position().top;
 
                 this.htmlCodeMirror = CodeMirror.fromTextArea(this.$el.find("#htmlArea")[0], {
                     mode: "application/xml",
+                    height: "350px",
                     lineNumbers: true,
+                    lineWrapping: true,
+                    viewportMargin: Infinity,
+                    iframeClass: 'CodeMirror'
                 });
-                codeMirrorBinder(this.model, "html", this.htmlCodeMirror);
 
-               this.helpersCodeMirror = CodeMirror.fromTextArea(this.$el.find("#helpersArea")[0], {
+                codeMirrorBinder(this.model, "html", this.htmlCodeMirror);
+                
+                $(this.htmlCodeMirror.getWrapperElement()).addClass(this.$el.find("#htmlArea").attr('class'));
+                $(this.htmlCodeMirror.getWrapperElement()).css("margin-top", topHtml);
+                
+                this.helpersCodeMirror = CodeMirror.fromTextArea(this.$el.find("#helpersArea")[0], {
                     mode: "javascript",
                     lineNumbers: true,
-               });
-               codeMirrorBinder(this.model, "helpers", this.helpersCodeMirror);
+                });
+                codeMirrorBinder(this.model, "helpers", this.helpersCodeMirror);
 
-                this.$el.find('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+                $(this.helpersCodeMirror.getWrapperElement()).addClass(this.$el.find("#helpersArea").attr('class'));
+                $(this.helpersCodeMirror.getWrapperElement()).css("margin-top", topHtml);
+
+                this.$el.find('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
                     self.helpersCodeMirror.refresh();
                     self.htmlCodeMirror.refresh();
                 });
@@ -37,25 +51,26 @@
 
                 self.$el.find("#previewFrameWrap").contents().find('html').html(
                     "<iframe name='previewFrame' frameborder='0' allowtransparency='true' allowfullscreen='true' style='width: 100%; height: 100%;'></iframe>");
-                
 
-                self.$el.find("[name=previewFrame]").on("load", function () {
-                        self.$el.find(".preview-loader").hide();
-                        //http://connect.microsoft.com/IE/feedback/details/809377/ie-11-load-event-doesnt-fired-for-pdf-in-iframe
-                        //$(this).show();
+
+                self.$el.find("[name=previewFrame]").on("load", function() {
+                    self.$el.find(".preview-loader").hide();
+                    //http://connect.microsoft.com/IE/feedback/details/809377/ie-11-load-event-doesnt-fired-for-pdf-in-iframe
+                    //$(this).show();
                 });
 
                 self.listenTo(app.layout, "dialog-opening", function() {
                     self.$el.find("[name=previewFrame]").hide();
                 });
-                
+
                 self.listenTo(app.layout, "dialog-closing", function() {
                     self.$el.find("[name=previewFrame]").show();
                 });
-                
+
 
                 this.$el.find(".split-pane").splitPane();
+                
+           
             },
         });
     });
-
