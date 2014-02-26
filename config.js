1be5f1f06ece88aafc0a9240dc027e15f7745b34
pofider <honza.pofider@seznam.cz>
2014-02-26 12:52:47 +0100

    add missing fonts

--- config.js
+++ config.js
@@ -2,15 +2,15 @@
     cookieSession: {
         key: 'jsreport.sid',
         secret: 'dasd321as56d1sd5s61vdv32',
-        cookie: { domain: 'jsreportonline.net' }
+        cookie: { domain: 'local.net' }
     },
     certificate: {
         key: 'config/jsreport.net.key',    
         cert: 'config/jsreport.net.cert'
     },
-    iisnode: true,
     connectionString: { name: "mongoDB", address: "localhost", port: 27017 },
     extensions: ["express", "templates", "html", "phantom-pdf", "fop-pdf", "scripts", "data", "images", "examples", "statistics", "reports", "jsreport-import-export"],
     mode: "multitenant",
-    port: process.env.PORT,
+    port: 3000,
+    httpPort: 4000
 }
