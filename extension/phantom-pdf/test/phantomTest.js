fbc0fa06d50cda489cf6db98b3175d48598a2004
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2015-04-07 17:45:06 +0200

    add phantom-pdf test

---
+++ extension/phantom-pdf/test/phantomTest.js
@@ -0,0 +1,25 @@
+﻿/*globals describe, it, beforeEach, afterEach */
+
+var assert = require("assert"),
+    fs = require('fs'),
+    path = require("path"),
+    _ = require("underscore"),
+    describeReporting = require("../../../test/helpers.js").describeReporting;
+
+describeReporting(path.join(__dirname, "../../"), ["html", "templates", "phantom-pdf"], function(reporter) {
+
+    describe('phantom pdf', function () {
+     
+        it('should not fail when rendering', function(done) {
+            var request = {
+                template: { content: "Heyx", recipe: "phantom-pdf", engine:"jsrender" },
+                data: null
+            };
+
+            reporter.render(request, {}).then(function(response) {
+                done();
+            }).catch(done);
+        });
+
+    });
+});
