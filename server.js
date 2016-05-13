6c634024420b19bc379028e3162a0af6676602b2
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-05-13 20:46:30 +0200

    update child templates

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
-require('./')({rootDirectory: __dirname}).init().catch(function (e) {
+var reporter = require('./')({rootDirectory: __dirname})
+
+var reporter = require('jsreport')()
+reporter.init().then(function () {
+  reporter.authorization.findPermissionFilteringListeners.add('readonly everything', function (collection, query, req) {
+    delete query.readPermissions
+  })
+}).catch(function (e) {
   console.trace(e)
   throw e
 })
+
+
+
+
+//
+//
+//var express = require('express');
+//var app = express();
+//
+//app.get('/', function (req, res) {
+// res.send('Hello from the main application');
+//});
+//
+//var reportingApp = express();
+//app.use('/reporting', reportingApp);
+//
+//var jsreport = require('./')({
+// express: { app :reportingApp },
+// rootDirectory: __dirname
+//});
+//
+//jsreport.init().then(function() {
+// app.listen(3000);
+//});
+
