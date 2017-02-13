a754d8e10f07a4d07dbebfaf0c38efee7101e3d1
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-02-13 13:50:11 +0100

    prepare for jsreport-compile

--- example.server.js
+++ example.server.js
@@ -1,9 +1,13 @@
 var jsreport = require('jsreport')()
 
-jsreport.init().then(function () {
-  // running
-}).catch(function (e) {
-  // error during startup
-  console.error(e.stack)
-  process.exit(1)
-})
+if (require.main !== module) {
+  module.exports = jsreport
+} else {
+  jsreport.init().then(function () {
+    // running
+  }).catch(function (e) {
+    // error during startup
+    console.error(e.stack)
+    process.exit(1)
+  })
+}
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-require('./')({rootDirectory: __dirname}).init().catch(function (e) {
-  console.trace(e)
-  process.exit(1)
-})
+var jsreport = require('./')({rootDirectory: __dirname})
+
+if (require.main !== module) {
+  module.exports = jsreport
+} else {
+  jsreport.init().then(function () {
+  }).catch(function (e) {
+    console.trace(e)
+    process.exit(1)
+  })
+}
