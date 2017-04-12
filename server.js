85407319e5896685f81ab46d63e8dc381abb6957
BJR Matos <bjrmatos@gmail.com>
2017-04-12 13:41:56 -0500

    update CLI

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var jsreport = require('./')({rootDirectory: __dirname})
 
-if (require.main !== module) {
+if (process.env.JSREPORT_CLI) {
   module.exports = jsreport
 } else {
   jsreport.init().then(function () {
