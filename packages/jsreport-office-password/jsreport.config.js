c4e2c199f30e32c111b97b4bad084c8dea6da9ff
BJR Matos <bjrmatos@gmail.com>
2021-08-10 14:25:57 -0500

    add jsreport-office-password

---
+++ packages/jsreport-office-password/jsreport.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'office-password',
+  dependencies: [],
+  main: 'lib/main.js',
+  worker: 'lib/worker.js',
+  optionsSchema: {},
+  requires: {
+    core: '2.x.x',
+    studio: '2.x.x'
+  }
+}
---
+++ packages/jsreport-office-password/lib/missingSecretMessage.js
@@ -0,0 +1,2 @@
+
+module.exports = 'office-password extension uses encryption to store sensitive data and needs secret key to be defined. Please fill "encryption.secretKey" at the root of the config or disable encryption using "encryption.enabled=false".'
