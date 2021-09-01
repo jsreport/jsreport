f159006d4d05e6f873a7933edbfba1112f98fff7
BJR Matos <bjrmatos@gmail.com>
2021-09-01 12:49:17 -0500

    TemplateWizard: don’t disable controls when there is options to create or select entity

--- packages/jsreport-studio/src/components/TemplateWizard/OfficeTemplateStep.js
+++ packages/jsreport-studio/src/components/TemplateWizard/OfficeTemplateStep.js
@@ -1,4 +1,5 @@
-import { useCallback } from 'react'
+import { useState, useCallback, useRef, useEffect } from 'react'
+import shortid from 'shortid'
 import FileInput from '../common/FileInput/FileInput'
 import EntityRefSelect from '../common/EntityRefSelect'
 
@@ -6,6 +7,8 @@ const officeRecipes = ['docx', 'pptx', 'html-to-xlsx', 'xlsx']
 
 function OfficeTemplateStep (props) {
   const { template, setAttributes, processing } = props
+  const [entityRefControlKey, setEntityRefControlKey] = useState(() => shortid.generate())
+  const mountedRef = useRef(false)
   const attrInfo = getAttributeForRecipe(template.recipe)
 
   let activeMode = 'new'
@@ -14,15 +17,29 @@ function OfficeTemplateStep (props) {
     activeMode = 'existing'
   }
 
-  const onModeChange = useCallback((ev) => {
-    const newMode = ev.target.value
-
+  const updateMode = useCallback(function updateMode (newMode) {
     if (newMode === 'new') {
       setAttributes({ [attrInfo.attrName]: null })
     } else if (newMode === 'existing') {
       setAttributes({ [attrInfo.attrName]: {} })
     }
   }, [setAttributes, attrInfo.attrName])
+
+  const onModeChange = useCallback((ev) => {
+    const newMode = ev.target.value
+    updateMode(newMode)
+  }, [updateMode])
+
+  useEffect(() => {
+    // reset entity ref control state
+    if (activeMode === 'new' && mountedRef.current) {
+      setEntityRefControlKey(shortid.generate())
+    }
+
+    if (!mountedRef.current) {
+      mountedRef.current = true
+    }
+  }, [activeMode])
 
   return (
     <div>
@@ -38,12 +55,14 @@ function OfficeTemplateStep (props) {
           />
           <span style={{ verticalAlign: 'middle' }}>Create new</span>
         </label>
-        <FileInput
-          placeholder={attrInfo.selectLabel}
-          selectedFile={template[attrInfo.attrName] != null ? template[attrInfo.attrName].file : undefined}
-          onFileSelect={(file) => setAttributes({ [attrInfo.attrName]: { file } })}
-          disabled={processing}
-        />
+        <div onClickCapture={() => { activeMode !== 'new' && updateMode('new') }}>
+          <FileInput
+            placeholder={attrInfo.selectLabel}
+            selectedFile={template[attrInfo.attrName] != null ? template[attrInfo.attrName].file : undefined}
+            onFileSelect={(file) => setAttributes({ [attrInfo.attrName]: { file } })}
+            disabled={processing}
+          />
+        </div>
       </div>
       <div className='form-group' style={{ marginTop: '1.2rem' }}>
         <label>
@@ -58,13 +77,13 @@ function OfficeTemplateStep (props) {
           <span style={{ verticalAlign: 'middle' }}>Select existing</span>
         </label>
         <div style={{ border: '1px dashed black', padding: '0.6rem' }}>
-          <div style={{ maxHeight: '20rem', overflow: 'auto' }}>
+          <div onClickCapture={() => { activeMode !== 'existing' && updateMode('existing') }} style={{ maxHeight: '20rem', overflow: 'auto' }}>
             <EntityRefSelect
+              key={entityRefControlKey}
               noModal
               treeStyle={{ minHeight: 'auto', maxHeight: 'none' }}
               headingLabel={attrInfo.selectLabel}
               filter={(references) => ({ data: references.assets })}
-              disabled={activeMode !== 'existing'}
               value={template[attrInfo.attrName] && template[attrInfo.attrName][attrInfo.shortidAttrName] != null ? template[attrInfo.attrName][attrInfo.shortidAttrName] : null}
               onChange={(selected) => {
                 setAttributes({ [attrInfo.attrName]: selected.length > 0 ? { [attrInfo.shortidAttrName]: selected[0].shortid } : {} })
