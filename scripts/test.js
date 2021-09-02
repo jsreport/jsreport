5c62c987556167687eba1a8120dae1b6b5822636
pofider <honza.pofider@seznam.cz>
2021-09-02 21:27:17 +0200

    rename to nodejs-client

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,7 @@ const execSync = require('child_process').execSync
 const jsreportPackages = [
   '@jsreport/advanced-workers',
   '@jsreport/odata-to-sql',
-  '@jsreport/client',
+  '@jsreport/nodejs-client',
   '@jsreport/jsreport-assets',
   '@jsreport/jsreport-authentication',
   '@jsreport/jsreport-authorization',
