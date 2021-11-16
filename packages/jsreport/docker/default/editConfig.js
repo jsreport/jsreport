67f1b2e3e4b27a211ece8ebc6953d28d04459a7c
pofider <honza.pofider@seznam.cz>
2021-11-16 13:40:33 +0100

    update full docker to run in azure and use edit config

--- packages/jsreport/docker/default/editConfig.js
+++ packages/jsreport/docker/default/editConfig.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const config = JSON.parse(fs.readFileSync('jsreport.config.json').toString())
+delete config.blobStorage
 delete config.extensions['sample-template']
 delete config.extensions.authentication
-fs.writeFileSync('jsreport.config.json', JSON.stringify(config))
+fs.writeFileSync('jsreport.config.json', JSON.stringify(config, null, 4))
