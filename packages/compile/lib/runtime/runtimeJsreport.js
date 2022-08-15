b63fa370f177c326e58055412825d3304bd0e288
pofider <honza.pofider@seznam.cz>
2022-08-15 22:59:47 +0200

    dont log to file system when binary is used

--- packages/compile/lib/runtime/runtimeJsreport.js
+++ packages/compile/lib/runtime/runtimeJsreport.js
@@ -1,4 +1,7 @@
 
+const { loggerFormat, createDefaultLoggerFormat } = require('@jsreport/jsreport-core')
+const defaultLoggerFormatWithTimestamp = createDefaultLoggerFormat({ timestamp: true })
+
 module.exports = (options) => {
   const coreOptions = {
     // we load config but don't do extensions discover by default
@@ -33,6 +36,39 @@ module.exports = (options) => {
     options.shortid,
     coreOptions.tempDirectory
   )
+
+  // dont use the jsreport default logs in the binary to avoid polluting work directory
+  // just when uer configures the logs console,file,error => apply the default settings to them as in jsreport
+  jsreport.afterConfigLoaded((reporter) => {
+    reporter.options.logger.console = Object.assign({
+      transport: 'console',
+      level: 'debug',
+      handleExceptions: true,
+      format: loggerFormat.combine(
+        loggerFormat.colorize(),
+        defaultLoggerFormatWithTimestamp()
+      )
+    }, reporter.options.logger.console)
+
+    if (reporter.options.logger.file) {
+      reporter.options.logger.file = Object.assign({
+        transport: 'file',
+        level: 'debug',
+        filename: 'logs/reporter.log',
+        maxsize: 10485760,
+        handleExceptions: true
+      }, reporter.options.logger.file)
+    }
+
+    if (reporter.options.logger.error) {
+      reporter.options.logger.error = Object.assign({
+        transport: 'file',
+        level: 'error',
+        filename: 'logs/error.log',
+        handleExceptions: true
+      }, reporter.options.logger.error)
+    }
+  })
 
   // enhance init function and add resources initialization
   const originalInit = jsreport.init.bind(jsreport)
