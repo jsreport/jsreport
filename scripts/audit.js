a853f35ff3509dfefb4a2a25719ebaa62d710a25
BJR Matos <bjrmatos@gmail.com>
2023-10-02 15:49:24 -0500

    add snyk test as part of audit script which executes as part of release process

--- scripts/audit.js
+++ scripts/audit.js
@@ -86,7 +86,7 @@ for (const ext of extensionsList) {
   console.log('\n===== dependencies install finished (logs above) =====\n')
 
   if (npmInstallError || npmInstallStatus === 1) {
-    console.error('Command failed to run')
+    console.error('npm install failed to run')
 
     if (npmInstallError) {
       throw npmInstallError
@@ -95,14 +95,14 @@ for (const ext of extensionsList) {
     process.exit(1)
   }
 
-  const { error: auditError, stdout, stderr, status: auditStatus } = spawnSync('npm', ['audit', '--omit', 'dev', '--workspaces=false'], {
+  const { error: auditError, stdout: auditStdout, stderr: auditStderr, status: auditStatus } = spawnSync('npm', ['audit', '--omit', 'dev', '--workspaces=false'], {
     cwd: path.join(process.cwd(), 'packages', packagesInWorkspace.get(ext)),
     stdio: 'pipe',
     shell: true
   })
 
-  const output = stdout != null ? stdout.toString().trim() : ''
-  const outputErr = stderr != null ? stderr.toString().trim() : ''
+  const auditOutput = auditStdout != null ? auditStdout.toString().trim() : ''
+  const auditOutputErr = auditStderr != null ? auditStderr.toString().trim() : ''
 
   if (auditError || auditStatus === 1) {
     console.error(`Audit command failed to run for ${ext}\n`)
@@ -111,27 +111,88 @@ for (const ext of extensionsList) {
       throw auditError
     }
 
-    if (output === '') {
-      if (outputErr !== '') {
-        console.error(outputErr)
+    if (auditOutput === '') {
+      if (auditOutputErr !== '') {
+        console.error(auditOutputErr)
       }
 
       process.exit(1)
     }
   }
 
-  if (output !== 'found 0 vulnerabilities') {
+  if (auditOutput !== 'found 0 vulnerabilities') {
     const outputPath = path.join(tempDir, `audit-${ext.replace(/\//g, '-')}.log`)
-    fs.writeFileSync(outputPath, output)
-    withVulnerabilities.push({ name: ext, output: outputPath })
+    fs.writeFileSync(outputPath, auditOutput)
+    withVulnerabilities.push({ name: ext, source: 'npm', output: outputPath })
+  }
+
+  // snyk test needs that we have done npm install first (which create package-lock.json),
+  // otherwise it reports vulnerabilities also of dev dependencies which is not what we want.
+  // in our case, we have done the install as the initial step, so we are fine.
+  // see https://docs.snyk.io/snyk-cli/getting-started-with-the-snyk-cli#scan-your-development-project
+  // and https://support.snyk.io/hc/en-us/articles/360015552617-Which-projects-must-be-built-before-testing-with-CLI-
+  // more useful links that describe snyk:
+  // - https://docs.snyk.io/scan-applications/snyk-open-source/manage-vulnerabilities/differences-in-open-source-vulnerability-counts-across-environments#differences-between-snyk-scans-and-other-tests
+  const snykTestArgs = ['snyk', 'test']
+
+  const { error: snykTestError, stdout: snykTestStdout, stderr: snykTestStderr, status: snykTestStatus } = spawnSync('npx', snykTestArgs, {
+    cwd: path.join(process.cwd(), 'packages', packagesInWorkspace.get(ext)),
+    stdio: 'pipe',
+    shell: true
+  })
+
+  const snykTestOutput = snykTestStdout != null ? snykTestStdout.toString().trim() : ''
+  const snykTestOutputErr = snykTestStderr != null ? snykTestStderr.toString().trim() : ''
+
+  // when snyk returns 1 it means, scan completed and vulnerability found,
+  // for the rest of case we assume error
+  // https://docs.snyk.io/snyk-cli/commands/test#exit-codes
+  if (snykTestStatus === 1) {
+    const outputPath = path.join(tempDir, `snyk-${ext.replace(/\//g, '-')}.log`)
+    fs.writeFileSync(outputPath, snykTestOutput)
+    withVulnerabilities.push({ name: ext, source: 'snyk', output: outputPath })
+  } else if (snykTestStatus !== 0) {
+    console.error('snyk test failed to run')
+
+    if (snykTestError) {
+      throw snykTestError
+    }
+
+    if (snykTestOutput !== '') {
+      console.error(snykTestOutput)
+    }
+
+    if (snykTestOutputErr !== '') {
+      console.error(snykTestError)
+    }
+
+    process.exit(1)
   }
 }
 
 console.log('===== Results =====')
 
 if (withVulnerabilities.length > 0) {
+  const groupedPackages = withVulnerabilities.reduce((acu, item) => {
+    if (!acu.has(item.name)) {
+      acu.set(item.name, [])
+    }
+
+    const sources = acu.get(item.name)
+
+    sources.push({
+      name: item.source,
+      output: item.output
+    })
+
+    return acu
+  }, new Map())
+
   console.log('\nThe following packages have vulnerabilities:')
-  console.log(`${withVulnerabilities.map((e) => `- ${e.name} (${e.output})`).join('\n')}`)
+
+  for (const [pkgName, pkgSources] of groupedPackages) {
+    console.log(`- ${pkgName}\n${pkgSources.map((source) => `   ${source.name}: ${source.output}`).join('\n')}\n`)
+  }
 } else {
   console.log('\nNo packages with vulnerabilities found')
 }
