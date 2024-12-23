228e921ea53dcc38be22e06a774a07539cda0a6f
BJR Matos <bjrmatos@gmail.com>
2024-12-23 18:08:34 -0500

    docx: fix docxHtml condition for possible null value

--- packages/jsreport-docx/lib/postprocess/html/convertDocxMetaToNodes.js
+++ packages/jsreport-docx/lib/postprocess/html/convertDocxMetaToNodes.js
@@ -348,25 +348,25 @@ module.exports = async function convertDocxMetaToNodes (reporter, docxMeta, html
             children: []
           })
 
-          if (currentDocxMeta.spacing.before != null) {
+          if (currentDocxMeta.spacing?.before != null) {
             tcMarEl.appendChild(
               createNode(doc, 'w:top', { attributes: { 'w:w': ptToTOAP(currentDocxMeta.spacing.before).toString(), 'w:type': 'dxa' } })
             )
           }
 
-          if (currentDocxMeta.indent.left != null) {
+          if (currentDocxMeta.indent?.left != null) {
             tcMarEl.appendChild(
               createNode(doc, 'w:left', { attributes: { 'w:w': ptToTOAP(currentDocxMeta.indent.left).toString(), 'w:type': 'dxa' } })
             )
           }
 
-          if (currentDocxMeta.spacing.after != null) {
+          if (currentDocxMeta.spacing?.after != null) {
             tcMarEl.appendChild(
               createNode(doc, 'w:bottom', { attributes: { 'w:w': ptToTOAP(currentDocxMeta.spacing.after).toString(), 'w:type': 'dxa' } })
             )
           }
 
-          if (currentDocxMeta.indent.right != null) {
+          if (currentDocxMeta.indent?.right != null) {
             tcMarEl.appendChild(
               createNode(doc, 'w:right', { attributes: { 'w:w': ptToTOAP(currentDocxMeta.indent.right).toString(), 'w:type': 'dxa' } })
             )
