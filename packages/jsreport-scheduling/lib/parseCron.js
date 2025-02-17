96b1424c9cc1a91dfa83394173fecb0dfda1cd04
pofider <jan.blaha@hotmail.com>
2025-02-17 19:29:33 +0100

    update cron-parser dep to support nth weekday of month like 0 0 * * 1#1

--- packages/jsreport-scheduling/lib/parseCron.js
+++ packages/jsreport-scheduling/lib/parseCron.js
@@ -1,4 +1,4 @@
-const cronParser = require('cron-parser')
+const { CronExpressionParser } = require('cron-parser')
 
 const cronPlaceholderRegExp = /^([^\s]+[\s]+)([^\s]+[\s]+)([^\s]+[\s]+)([^\s]+[\s]+)([^\s]+[\s]*)([^\s]+[\s]*)?$/
 
@@ -7,5 +7,5 @@ module.exports = function (cron, options = {}) {
     throw new Error('Cron expression should have only 5 or 6 parts')
   }
 
-  return cronParser.parseExpression(cron, options)
+  return CronExpressionParser.parse(cron, options)
 }
