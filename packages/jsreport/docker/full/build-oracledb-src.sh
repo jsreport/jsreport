11f7e48312738d6b25bf960b65c298d50e04c698
BJR Matos <bjrmatos@gmail.com>
2025-03-11 22:59:05 -0500

    fix docker full arm build, fix oracledb compilation

--- packages/jsreport/docker/full/build-oracledb-src.sh
+++ packages/jsreport/docker/full/build-oracledb-src.sh
@@ -3,17 +3,16 @@
 set -e
 
 rm -rf /app/node_modules/oracledb
-curl -L https://github.com/oracle/node-oracledb/releases/download/v$1/oracledb-src-$1.tgz > oracledb-src.tgz
-mkdir /app/oracledb-src
-tar -xzf oracledb-src.tgz -C /app/oracledb-src
-cd /app/oracledb-src/package
+git clone --recurse-submodules https://github.com/oracle/node-oracledb /app/oracledb-src
+cd /app/oracledb-src
+git checkout tags/v$1
+git submodule update
 npm run buildbinary
 npm run buildpackage
 mkdir oracledb-$1
 tar -xzf oracledb-$1.tgz -C oracledb-$1
 mkdir /app/node_modules/oracledb
-mv -v /app/oracledb-src/package/oracledb-$1/package/* /app/node_modules/oracledb
-cd /app
-rm -f oracledb-src.tgz
+mv -v /app/oracledb-src/oracledb-$1/package/* /app/node_modules/oracledb
 rm -rf /app/oracledb-src
+cd /app
 echo "finished oracledb build"
