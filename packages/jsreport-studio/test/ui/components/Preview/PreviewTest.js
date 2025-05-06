4fe799f4daabd1fae5abe692665dfcc8362952e8
BJR Matos <bjrmatos@gmail.com>
2025-05-06 15:41:09 -0500

    studio: migrate from Enzyme to fix tests

--- packages/jsreport-studio/test/ui/components/Preview/PreviewTest.js
+++ packages/jsreport-studio/test/ui/components/Preview/PreviewTest.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import should from 'should'
 import Preview from '../../../../src/components/Preview/Preview'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-Enzyme.configure({ adapter: new Adapter() })
+import { render } from '@testing-library/react'
 
 // don't find out such kind of component tests very much useful, but contributions welcome
 describe('<Preview />', () => {
-  it('calls componentDidMount', () => {
-    should(shallow(<Preview />).find('.block')).have.lengthOf(1)
+  it('can render', () => {
+    const { container } = render(<Preview />)
+    // this is not the recommend way to assert with testing-library
+    // for our purpose (of only 1 test) it is fine
+    const blockElement = container.querySelector('.block')
+    should(blockElement).be.ok()
   })
 })
